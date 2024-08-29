664d7ae6ed036b0afebb9a5dbe81c090168efeff
Jason Tsai <80815982+Khaos991213@users.noreply.github.com>
2024-08-29 17:01:50 +0800

    Update game.py

--- game.py
+++ game.py
@@ -25,57 +25,63 @@ class Maze:
         pygame.display.set_caption("Maze Game")
         self.clock = pygame.time.Clock()
         self.rendering_enabled=rendering_enabled
-        self.maze =[[5, 3, 0, 0, 0, 0, 0, 3, 1, 3, 1, 1, 0, 1, 1, 3], 
-                    [0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0], 
-                    [0, 3, 1, 0, 1, 0, 0, 0, 0, 3, 1, 1, 3, 1, 1, 0], 
-                    [0, 1, 1, 0, 1, 3, 1, 1, 1, 0, 0, 0, 1, 3, 0 ,0], 
-                    [0, 0, 0, 0, 3, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0], 
-                    [1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 3, 0, 0], 
-                    [3, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0], 
-                    [1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 3, 0, 0, 1, 0], 
-                    [0, 0, 1, 0, 0, 1, 0, 1, 3, 1, 0, 1, 1, 1, 3, 0], 
-                    [1, 0, 1, 0, 1, 1, 3, 0, 0, 1, 0, 0, 0, 0, 1, 0], 
-                    [3, 0, 1, 0, 0, 3, 1, 1, 0, 1, 1, 1, 1, 3, 1, 0], 
-                    [1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0], 
-                    [1, 0, 3, 0, 1, 0, 1, 0, 1, 1, 1, 3, 1, 0, 3, 0], 
-                    [3, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0], 
-                    [1, 1, 1, 0, 1, 1, 3, 1, 1, 0, 1, 0, 1, 0, 1, 0], 
-                    [3, 0, 0, 0, 0, 3, 1, 1, 1, 0, 3, 1, 3, 0, 0, 2]]
+        self.maze =[[6, 4, 1, 1, 1, 1, 1, 4, 2, 4, 2, 2, 1, 2, 2, 4], 
+                    [1, 2, 2, 1, 2, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1], 
+                    [1, 4, 2, 1, 2, 1, 1, 1, 1, 4, 2, 2, 4, 2, 2, 1], 
+                    [1, 2, 2, 1, 2, 4, 2, 2, 2, 1, 1, 1, 2, 4, 1, 1], 
+                    [1, 1, 1, 1, 4, 1, 1, 1, 1, 2, 2, 1, 2, 2, 2, 1], 
+                    [2, 1, 2, 2, 2, 2, 1, 2, 1, 2, 2, 1, 2, 4, 1, 1], 
+                    [4, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 2, 1, 2, 1], 
+                    [2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 4, 1, 1, 2, 1], 
+                    [1, 1, 2, 1, 1, 2, 1, 2, 4, 2, 1, 2, 2, 2, 4, 1], 
+                    [2, 1, 2, 1, 2, 2, 4, 1, 1, 2, 1, 1, 1, 1, 2, 1], 
+                    [4, 1, 2, 1, 1, 4, 2, 2, 1, 2, 2, 2, 2, 4, 2, 1], 
+                    [2, 1, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1], 
+                    [2, 1, 4, 1, 2, 1, 2, 1, 2, 2, 2, 4, 2, 1, 4, 1], 
+                    [4, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1], 
+                    [2, 2, 2, 1, 2, 2, 4, 2, 2, 1, 2, 1, 2, 1, 2, 1], 
+                    [4, 1, 1, 1, 1, 4, 2, 2, 2, 1, 4, 2, 4, 1, 1, 3]]
         self.player_x = 0
         self.player_y = 0
         self.player_score = 0
         self.timer = Timer(countdown_time)
-     
+        self.visited = set()
+        self.visited.add((self.player_y, self.player_x))  # Add the coordinates as a tuple
+
         self.monster_x = 8 
         self.monster_y = 9
         self.prev_mx = self.monster_x
         self.prev_my = self.monster_y
         self.monster_prev_tile = self.monster_prev_tile = self.maze[self.monster_y][self.monster_x]  # To save the previous tile where the monster
-        
+
+        self.no_move_count = 0  # Track the number of steps without movement
+        self.max_no_move_steps = 10  # Define a threshold for no movement
         self.update_maze_with_monster()
         self.update_maze_with_player()  # 更新玩家位置
     
     def reset(self):
-        self.maze =[[5, 3, 0, 0, 0, 0, 0, 3, 1, 3, 1, 1, 0, 1, 1, 3], 
-                    [0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0], 
-                    [0, 3, 1, 0, 1, 0, 0, 0, 0, 3, 1, 1, 3, 1, 1, 0], 
-                    [0, 1, 1, 0, 1, 3, 1, 1, 1, 0, 0, 0, 1, 3, 0 ,0], 
-                    [0, 0, 0, 0, 3, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0], 
-                    [1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 3, 0, 0], 
-                    [3, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0], 
-                    [1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 3, 0, 0, 1, 0], 
-                    [0, 0, 1, 0, 0, 1, 0, 1, 3, 1, 0, 1, 1, 1, 3, 0], 
-                    [1, 0, 1, 0, 1, 1, 3, 0, 0, 1, 0, 0, 0, 0, 1, 0], 
-                    [3, 0, 1, 0, 0, 3, 1, 1, 0, 1, 1, 1, 1, 3, 1, 0], 
-                    [1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0], 
-                    [1, 0, 3, 0, 1, 0, 1, 0, 1, 1, 1, 3, 1, 0, 3, 0], 
-                    [3, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0], 
-                    [1, 1, 1, 0, 1, 1, 3, 1, 1, 0, 1, 0, 1, 0, 1, 0], 
-                    [3, 0, 0, 0, 0, 3, 1, 1, 1, 0, 3, 1, 3, 0, 0, 2]]
+        self.maze =[[6, 4, 1, 1, 1, 1, 1, 4, 2, 4, 2, 2, 1, 2, 2, 4], 
+                    [1, 2, 2, 1, 2, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1], 
+                    [1, 4, 2, 1, 2, 1, 1, 1, 1, 4, 2, 2, 4, 2, 2, 1], 
+                    [1, 2, 2, 1, 2, 4, 2, 2, 2, 1, 1, 1, 2, 4, 1, 1], 
+                    [1, 1, 1, 1, 4, 1, 1, 1, 1, 2, 2, 1, 2, 2, 2, 1], 
+                    [2, 1, 2, 2, 2, 2, 1, 2, 1, 2, 2, 1, 2, 4, 1, 1], 
+                    [4, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 2, 1, 2, 1], 
+                    [2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 4, 1, 1, 2, 1], 
+                    [1, 1, 2, 1, 1, 2, 1, 2, 4, 2, 1, 2, 2, 2, 4, 1], 
+                    [2, 1, 2, 1, 2, 2, 4, 1, 1, 2, 1, 1, 1, 1, 2, 1], 
+                    [4, 1, 2, 1, 1, 4, 2, 2, 1, 2, 2, 2, 2, 4, 2, 1], 
+                    [2, 1, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1], 
+                    [2, 1, 4, 1, 2, 1, 2, 1, 2, 2, 2, 4, 2, 1, 4, 1], 
+                    [4, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1], 
+                    [2, 2, 2, 1, 2, 2, 4, 2, 2, 1, 2, 1, 2, 1, 2, 1], 
+                    [4, 1, 1, 1, 1, 4, 2, 2, 2, 1, 4, 2, 4, 1, 1, 3]]
         self.player_x = 0
         self.player_y = 0
         self.player_score = 0
         self.timer = Timer(self.timer.countdown_time)
+        self.visited = set()
+        self.visited.add((self.player_y, self.player_x))  # Add the coordinates as a tuple
 
         self.monster_x = 8 
         self.monster_y = 9
@@ -113,31 +119,31 @@ class Maze:
         for _ in range(50):  # Randomly add obstacles
             x = random.randint(0, MAZE_WIDTH - 1)
             y = random.randint(0, MAZE_HEIGHT - 1)
-            maze[y][x] = 1
+            maze[y][x] = 2
         
         for _ in range(15):  # Randomly add points (incentives)
             while True:
                 x = random.randint(0, MAZE_WIDTH - 1)
                 y = random.randint(0, MAZE_HEIGHT - 1)
-                if maze[y][x] == 0:
-                    maze[y][x] = 3 
+                if maze[y][x] == 1:
+                    maze[y][x] = 4 
                     break
         
-        maze[MAZE_HEIGHT - 1][MAZE_WIDTH - 1] = 2  
+        maze[MAZE_HEIGHT - 1][MAZE_WIDTH - 1] = 3  
         return maze
     
     def draw_maze(self):
         for y in range(MAZE_HEIGHT):
             for x in range(MAZE_WIDTH):
-                if self.maze[y][x] == 1: #Start
+                if self.maze[y][x] == 2: #Start
                     pygame.draw.rect(self.screen, BLACK, (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
-                elif self.maze[y][x] == 2: # End 
+                elif self.maze[y][x] == 3: # End 
                     pygame.draw.rect(self.screen, RED, (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
-                elif self.maze[y][x] == 3: # Points
+                elif self.maze[y][x] == 4: # Points
                     pygame.draw.circle(self.screen, BLUE, (x * CELL_SIZE + CELL_SIZE // 2, y * CELL_SIZE + CELL_SIZE // 2), CELL_SIZE // 4)
-                elif self.maze[y][x] == 4: # Monster 
+                elif self.maze[y][x] == 5: # Monster 
                     pygame.draw.rect(self.screen, GRAY, (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE))  
-                elif self.maze[y][x] == 5: # Player
+                elif self.maze[y][x] == 6: # Player
                     pygame.draw.rect(self.screen, GREEN, (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
 
     def update_maze_with_monster(self):
@@ -148,27 +154,26 @@ class Maze:
         # Update the monster's position on the maze
         if self.monster_x == self.player_x and self.monster_y == self.player_y:
             return  # Handle the case where the monster collides with the player
-        self.maze[self.monster_y][self.monster_x] = 4  # Assuming 4 is the value for the monster
+        self.maze[self.monster_y][self.monster_x] = 5  # Assuming 4 is the value for the monster
 
         if self.monster_x == self.player_x and self.monster_y == self.player_y:
             return  # Handle the case where the monster collides with the player
         
         
         
-        self.maze[self.monster_y][self.monster_x] = 4  # '4' represents the monster
+        self.maze[self.monster_y][self.monster_x] = 5  # '4' represents the monster
 
     def update_maze_with_player(self):
         # Ensure the previous player position is marked as empty
         for y in range(MAZE_HEIGHT):
             for x in range(MAZE_WIDTH):
-                if self.maze[y][x] == 5:
-                    self.maze[y][x] = 0
-        # Check if the player has reached the end (maze[y][x] == 2)
-        if self.maze[self.player_y][self.player_x] == 2:
+                if self.maze[y][x] == 6:
+                    self.maze[y][x] = 1
+        # Check if the player has reached the end (maze[y][x] == 3)
+        if self.maze[self.player_y][self.player_x] == 3:
             return
         # Set the new player position in the maze
-        self.maze[self.player_y][self.player_x] = 5
-
+        self.maze[self.player_y][self.player_x] = 6
     def move_monster(self):
         # Randomly choose a direction for the monster
         directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
@@ -179,7 +184,7 @@ class Maze:
             new_x = self.monster_x + dx
             new_y = self.monster_y + dy
             
-            if 0 <= new_x < MAZE_WIDTH and 0 <= new_y < MAZE_HEIGHT and self.maze[new_y][new_x] != 1:
+            if 0 <= new_x < MAZE_WIDTH and 0 <= new_y < MAZE_HEIGHT and self.maze[new_y][new_x] != 2:
                 # Move the monster to the new position
                 self.prev_mx = self.monster_x
                 self.prev_my = self.monster_y
@@ -202,49 +207,56 @@ class Maze:
         
         new_x = self.player_x + dx
         new_y = self.player_y + dy
-        reward = -5
+        reward = -0.05
         if 0 <= new_x < MAZE_WIDTH and 0 <= new_y < MAZE_HEIGHT:
-            if self.maze[new_y][new_x] != 1:
+            if self.maze[new_y][new_x] != 2:
+                self.no_move_count = 0
                 # Update player position
                 self.player_x = new_x
                 self.player_y = new_y
                 
+                # Add the current position to the visited set
+                if (self.player_y, self.player_x) in self.visited:
+                    print("Visited!")
+                    reward -= 0.2  # Penalty for revisiting a position
+                else:
+                    print("Added")
+                    self.visited.add((self.player_y, self.player_x))
+                
                 # Collect points
-                if self.maze[self.player_y][self.player_x] == 3:
+                if self.maze[self.player_y][self.player_x] == 4:
                     self.player_score += 10  # Increase score by 10 for each point
-                    reward += 150
-                    self.maze[self.player_y][self.player_x] = 0  # Remove the point from the maze
-                if self.maze[self.player_y][self.player_x] == 2:
+                    reward += 5
+                    self.maze[self.player_y][self.player_x] = 1  # Remove the point from the maze
+                if self.maze[self.player_y][self.player_x] == 3:
                     self.player_score += 100
                     reward += 100
-                if self.maze[self.player_y][self.player_x] == 4:
-                    reward = -150  # Penalty for losing
+                if self.maze[self.player_y][self.player_x] == 5:
+                    self.player_score -= 500
+                    reward = -10  # Penalty for losing
                     done = True  # End game when colliding with the monster
                     self.render()  # Update the display
                     return self.get_state(), reward, done  # Return immediately if collision occurs
             else:
-                reward -= 20
+                reward -= 0.75
+                self.no_move_count += 1 
             # Update the maze with the new player position
-            self.update_maze_with_player()
         else:
-            reward-=20
-        # Move the monster after the player's movement
+            reward -= 0.75
+            self.no_move_count += 1
+        self.update_maze_with_player()
         self.move_monster()
-        
-        # Check if the game is won or if time is up
-        done = (self.maze[self.player_y][self.player_x] == 2 or
+
+        done = (self.maze[self.player_y][self.player_x] == 3 or
                 self.monster_x == self.player_x and self.monster_y == self.player_y or
                 self.timer.is_time_up())
-        
-        # If the player touches the monster after the monster's movement, the game is lost
-        if self.monster_x == self.player_x and self.monster_y == self.player_y:
-            reward = -500  # Penalty for losing
+
+        if self.no_move_count >= self.max_no_move_steps:
+            reward = -10  # Penalty for staying in the same position
             done = True
-        
         self.render()
         state = self.get_state()
         return state, reward, done
-    
     def run(self):
         running = True
         won = False
@@ -262,8 +274,12 @@ class Maze:
                         self.step(2)
                     elif event.key == pygame.K_RIGHT:
                         self.step(3)
+            # print(f'3:{self.no_move_count}')
+            if self.no_move_count >= self.max_no_move_steps:
+                won = True
+                running=False
 
-            if self.maze[self.player_y][self.player_x] == 2:
+            if self.maze[self.player_y][self.player_x] == 3:
                 won = True
                 running=False
             if self.monster_x == self.player_x and self.monster_y == self.player_y:
